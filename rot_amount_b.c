0e07bd9cf70f1dc96cad8fab3d2503bced39bde0
surajbartoula <suraj_bartoula@yahoo.com>
2024-05-21 04:56:05 +0000

    modifying the rot_amount

--- rot_amount_b.c
+++ rot_amount_b.c
@@ -6,7 +6,7 @@
 /*   By: sbartoul <sbartoul@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/18 18:00:27 by sbartoul          #+#    #+#             */
-/*   Updated: 2024/05/21 04:54:31 by sbartoul         ###   ########.fr       */
+/*   Updated: 2024/05/21 04:55:32 by sbartoul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,7 @@ int	rrarb_b(t_stack *a, t_stack *b, int num)
 {
 	int	i;
 
-	if (ft_find_index(a, num))
+	if (ft_find_index(a, num) > 0)
 		i = ft_lstsize(a) - ft_find_index(a, num);
 	else
 		i = 0;
