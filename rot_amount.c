aac2884cbc60a933d27c73962605d98072a781dc
surajbartoula <suraj_bartoula@yahoo.com>
2024-05-21 05:03:06 +0000

    still fixing

--- rot_amount.c
+++ rot_amount.c
@@ -6,7 +6,7 @@
 /*   By: sbartoul <sbartoul@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/18 13:55:33 by sbartoul          #+#    #+#             */
-/*   Updated: 2024/05/19 11:13:49 by sbartoul         ###   ########.fr       */
+/*   Updated: 2024/05/21 05:02:30 by sbartoul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,44 +14,44 @@
 
 int	rarb_a(t_stack *a, t_stack *b, int num)
 {
-	int	i;
-
-	i = ft_find_place_a(a, num);
-	if (i < ft_find_index(b, num))
-		i = ft_find_index(b, num);
-	return (i);
+	if (ft_find_place_a(a, num) < ft_find_index(b, num))
+		return (ft_find_index(b, num));
+	else
+		return (ft_find_place_a(a, num));
 }
 
 int	rrarrb_a(t_stack *a, t_stack *b, int num)
 {
 	int	i;
 
-	i = 0;
-	if (ft_find_place_a(a, num))
+	if (ft_find_place_a(a, num) > 0)
 		i = ft_lstsize(a) - ft_find_place_a(a, num);
+	else
+		i = 0;
 	if (ft_find_index(b, num) && (i < (ft_lstsize(b) - ft_find_index(b, num))))
-		i = ft_lstsize(b) - ft_find_index(b, num);
-	return (i);
+		return (ft_lstsize(b) - ft_find_index(b, num));
+	else
+		return (i);
 }
 
 int	rarrb_a(t_stack *a, t_stack *b, int num)
 {
 	int	i;
 
-	i = 0;
-	if (ft_find_index(b, num))
+	if (ft_find_index(b, num) > 0)
 		i = ft_lstsize(b) - ft_find_index(b, num);
-	i = ft_find_place_a(a, num) + i;
-	return (i);
+	else
+		i = 0;
+	return (ft_find_place_a(a, num) + i);
 }
 
 int	rrarb_a(t_stack *a, t_stack *b, int num)
 {
 	int	i;
 
-	i = 0;
-	if (ft_find_place_a(a, num))
+	if (ft_find_place_a(a, num) > 0)
 		i = ft_lstsize(a) - ft_find_place_a(a, num);
-	i = ft_find_index(b, num) + i;
-	return (i);
+	else
+		i = 0;
+	return (ft_find_index(b, num) + i);
 }
